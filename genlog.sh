847a16d34d28001fced6bdf661d8261e512865f5
OniriCorpe <oniricorpe@disroot.org>
2022-05-06 22:53:02 +0200

    désambiguisation de variables et fix nom fichier écrit

--- genlog.sh
+++ genlog.sh
@@ -32,16 +32,16 @@ do
     gmnitohtml < "$gmi_file" > "$tempdir/body.html"
 
     # on récupère juste le path du dossier qui contient le .gmi
-    path="$(dirname "$gmi_file")"
+    file_path="$(dirname "$gmi_file")"
     # on récupère juste le nom du fichier .gmi sans son extenstion ".gmi"
-    filename="$(basename "$gmi_file" .gmi)"
+    file_name="$(basename "$gmi_file" .gmi)"
 
     # on assemble les 3 morceaux et on l'écrit dans le dossier du .gmi qui est traité
-    cat "$tempdir/header.html" "$tempdir/body.html" "$tempdir/footer.html" > "$path/$filename.html"
+    cat "$tempdir/header.html" "$tempdir/body.html" "$tempdir/footer.html" > "$file_path/$file_name.html"
 
     # je crois c'est bon
     echo "OK: $title"
-    echo "    ⤷ $gmi_file"
+    echo "    ⤷ $file_path/$file_name.html"
 
 done
 
